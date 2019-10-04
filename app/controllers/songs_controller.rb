06417bd145b3811a25494f9690521664c28360d8
Elana Ryznar <eryzna@gmail.com>
2019-10-04 22:14:48 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,17 +1,21 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
+  use Rack::Flash
 
   get '/songs' do
     @songs = Song.all
     erb :'/songs/index'
   end
 
-  get '/songs/:slug' do
-    @song = Song.find_by_slug(params[:slug])
-    erb :'songs/show'
-  end
-
   get '/songs/new' do
     erb :'/songs/new'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+
+    erb :'songs/show'
   end
 
   post '/songs' do
@@ -20,8 +24,26 @@ class SongsController < ApplicationController
     @song.genre_ids = params[:genres]
     @song.save
 
-    #flash[:message] = "Successfully created song."
+    flash[:message] = "Successfully created song."
 
     redirect("/songs/#{@song.slug}")
   end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genre_ids = params[:genres]
+    @song.save
+
+    flash[:message] = "Successfully updated song."
+    redirect("/songs/#{@song.slug}")
+  end
+
 end
+© 2019 GitHub, Inc.
